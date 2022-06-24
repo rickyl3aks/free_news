b7c2151599f6ca33890597c5e7528720e668643a
Riccardo Consolandi <rconsolandi@gmail.com>
2022-06-24 13:26:29 +0200

    refactor: loading.tsx; change skeleton loading

--- src/loading/Loading.tsx
+++ src/loading/Loading.tsx
@@ -1,10 +1,16 @@
 import * as React from "react";
 import { Skeleton } from "@mui/material";
+import { Card } from "@mui/material";
 
 export const Loading = () => {
   let skeleton = [];
   for (let n: number = 1; n <= 2; n++) {
     skeleton.push(n);
+  }
+
+  let rec: number[] = [];
+  for (let n: number = 1; n <= 2; n++) {
+    rec.push(n);
   }
 
   return (
@@ -18,17 +24,33 @@ export const Loading = () => {
     >
       {skeleton.map((e: number) => {
         return (
-          <Skeleton
+          <Card
             key={"key_" + e}
-            animation="wave"
             sx={{
+              maxWidth: 350,
               borderRadius: "0.3rem",
               margin: "1rem",
             }}
-            variant="rectangular"
-            width={363}
-            height={280}
-          />
+          >
+            <Skeleton
+              animation="wave"
+              variant="rectangular"
+              width={363}
+              height={180}
+            />
+            <Skeleton animation="wave" height={20} style={{ margin: 8 }} />
+            {rec.map((e: number) => {
+              return (
+                <Skeleton
+                  key={"key_" + e}
+                  animation="wave"
+                  height={20}
+                  width="80%"
+                  style={{ margin: 8 }}
+                />
+              );
+            })}
+          </Card>
         );
       })}
     </div>
